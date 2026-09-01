6d943acb623ad4a9805bf782718814f1d62cc55b
Natan Furletti <128628722+NatanFurletti@users.noreply.github.com>
2026-09-01 22:35:36 +0000

    two sum solution

---
+++ 1. Two Sum.js
@@ -0,0 +1,20 @@
+var twoSum = function (nums, target) {
+  let n = 0;
+  let m = 1;
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[n] + nums[m] === target) {
+      result.push(n);
+      result.push(m);
+      console.log(result);
+      return result;
+    } else if (m < nums.length) {
+      m++;
+    } else {
+      i = 0;
+      n++;
+      m = n + 1;
+      console.log(n);
+    }
+  }
+};
